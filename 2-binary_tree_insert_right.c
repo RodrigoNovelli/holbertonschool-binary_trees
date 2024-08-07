668feb50aba6421b59314ad12690517731fb25d9
RodrigoNovelli <Rodrigonovelli21@hotmail.com>
2024-08-07 00:52:27 +0000

    If parent is null case

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -12,6 +12,8 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	binary_tree_t *current;
 	binary_tree_t *rightch;
 
+	if(parent == NULL)
+		return (NULL);
 	current = parent;
 	rightch = binary_tree_node(current, value);
 	if (rightch == NULL)
