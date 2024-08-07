81148f33da2d4ceef3c3972a717e795740bde082
juanidaste <9343@holbertonstudents.com>
2024-08-07 16:00:35 +0000

    Solving Betty

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 /**
- * binary_tree_delete - Write a function that checks if a node is a leaf.
- * @node: The node you want to check if itÂs a leaf. 
+ * binary_tree_is_leaf - Write a function that checks if a node is a leaf.
+ * @node: The node you want to check if itÂs a lea.
  * Return: 0 if node ins NULL and 1 if node is a leaf.
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
