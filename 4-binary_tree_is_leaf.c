0fd8d8f6c930dfa1a2ef4ef9ce8040f1d4e7369b
juanidaste <9343@holbertonstudents.com>
2024-08-07 15:59:08 +0000

    Fixing return error

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,9 +1,8 @@
 #include "binary_trees.h"
 /**
- * binary_tree_delete - Write a function that checks if a node is a leaf}
- * @node: 
- * Retunr: 0 if node ins NULL and 1 if node is a leaf
- *
+ * binary_tree_delete - Write a function that checks if a node is a leaf.
+ * @node: The node you want to check if itÂs a leaf. 
+ * Return: 0 if node ins NULL and 1 if node is a leaf.
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 
@@ -11,7 +10,7 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 	if (node == NULL)
 	return (0);
 
-	if (node->parent != NULL)
+	if (node->left == NULL &&  node->right == NULL)
 		return (1);
 	return (0);
 }
