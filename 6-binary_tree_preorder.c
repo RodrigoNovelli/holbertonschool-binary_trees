e42d3ffaf4f0321615be86bdd8ac65eb8e0f911a
juanidaste <9343@holbertonstudents.com>
2024-08-07 17:53:37 +0000

    6-binary_tree_preorder.c

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,12 +1,15 @@
 #include "binary_trees.h"
 /**
- *
+ * binary_tree_preorder - Iterates every way of a binary tree.
+ * @tree: The root of the binary tree.
+ * @func : -
+ * Retrun: void
 **/
-
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (!tree || !func) return;
-       		func(tree->n);
-	       	binary_tree_preorder(tree->left, func);
-	       	binary_tree_preorder(tree->right, func);
+	if (!tree || !func)
+		return;
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
